2a728ffab33f427cb769293e2f1a954c07acd859
Conan Gammel <Cgammel@Conans-MacBook-Pro-2.local>
2016-07-05 16:00:12 -0500

    UI template almost done

---
+++ BluemixMobileServicesDemoApp/BluemixMobileServicesDemoApp/EmailLinksController.swift
@@ -0,0 +1,9 @@
+//
+//  EmailLinksController.swift
+//  BluemixMobileServicesDemoApp
+//
+//  Created by Conan Gammel on 7/5/16.
+//  Copyright © 2016 Conan Gammel. All rights reserved.
+//
+
+import Foundation
