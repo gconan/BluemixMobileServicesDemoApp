a684a47eacc1da89de5b3c462ba8bba2b6906ce9
Conan Gammel <Cgammel@Conans-MacBook-Pro-2.local>
2016-07-07 10:29:43 -0500

    included the watson sdks, added an app icon, fixing the storyboard

--- BluemixMobileServicesDemoApp/BluemixMobileServicesDemoApp/ViewController.swift
+++ BluemixMobileServicesDemoApp/BluemixMobileServicesDemoApp/ViewController.swift
@@ -19,11 +19,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func yodaquote(sender: AnyObject) {
-    }
-
-    @IBAction func darthVader(sender: AnyObject) {
-    }
 }
 
+/*
+ "If you let my daughter go now, that'll be the end of it. I will not look for you, I will not pursue you. But if you don't, I will look for you, I will find you, and I will kill you." - Taken
+ 
+ "My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions and loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, in this life or the next." -Gladiator
+ 
+ "Momma says, alligators are so angry because they got all them teeth and no toothbrush" -Waterboy
+ 
+ "You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya punk?" - Dirty Harry
+ 
+ “What? You pooped in the refrigerator? And you ate the whole wheel of cheese? How’d you do that? Heck, I’m not even mad; that’s amazing.” — Ron Burgundy, Anchorman
+ 
+ “Luke, you can destroy the Emperor. He has foreseen this. It is your destiny. Join me, and together we can rule the galaxy as father and son.” -DarthVader StarWars
+ 
+ "Don't get technical with me. What mission? What are you talking about? I've just about had enough of you. Go that way, you'll be malfunctioning in a day you near-sighted scrap pile." -C-3PO StarWars
+ */
